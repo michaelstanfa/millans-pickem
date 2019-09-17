1004617f20225c1cdbc402d1d0a3797d3a97e05e
Michael Stanfa <stanfa.michael@gmail.com>
2019-09-16 22:22:52 -0500

    committing
    got
    exit
    q
    q
    wc
    Q
    ^q
    quite
    quit

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+const loadThisWeekData = (date) => {
+
+	response = $.ajax
+	({
+		type: "GET",
+		url: "https://api.mysportsfeeds.com/v1.2/pull/nfl/2019-regular/full_game_schedule.json",
+		dataType: 'json',
+		async: true,
+		headers: {
+	      "Authorization": "Basic " + btoa(creds.id + ":" + creds.secret)
+	    },
+	    success: function (){
+	    	console.log(JSON.parse(response.responseText));
+	   	},
+	   	error: function(XMLHttpRequest, textStatus, errorThrown) {
+	   		console.log("Failed to make call to endpoint");
+	   		console.error("Status: " + textStatus);
+	   		console.error("Error: " + errorThrown);
+	   	}
+
+	}) 
+}
