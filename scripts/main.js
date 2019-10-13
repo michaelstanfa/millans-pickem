b67167915de7bf3365762640bc651e0d5788ec29
Stanfa, Michael <Stanfa.Michael@principal.com>
2019-10-13 13:29:33 -0500

    update

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,31 @@
-const loadThisWeekData = (date) => {
+let schedule = null;
 
-	response = $.ajax
-	({
-		type: "GET",
-		url: "https://api.mysportsfeeds.com/v1.2/pull/nfl/2019-regular/full_game_schedule.json",
-		dataType: 'json',
-		async: true,
-		headers: {
-	      "Authorization": "Basic " + btoa(creds.id + ":" + creds.secret)
-	    },
-	    success: function (){
-	    	console.log(JSON.parse(response.responseText));
-	   	},
-	   	error: function(XMLHttpRequest, textStatus, errorThrown) {
-	   		console.log("Failed to make call to endpoint");
-	   		console.error("Status: " + textStatus);
-	   		console.error("Error: " + errorThrown);
-	   	}
+const loadSpecificWeekMatchups = (week) => {
 
-	}) 
+	if(null == schedule) {
+		console.log(week);
+		response = $.ajax
+		({
+			type: "GET",
+			url: "https://api.mysportsfeeds.com/v1.2/pull/nfl/2019-regular/full_game_schedule.json",
+			dataType: 'json',
+			async: true,
+			headers: {
+		      "Authorization": "Basic " + btoa(creds.id + ":" + creds.secret)
+		    },
+		    success: function (){
+		    	schedule = JSON.parse(response.responseText);
+		    	console.log(JSON.parse(response.responseText));
+		    	return schedule;
+		   	},
+		   	error: function(XMLHttpRequest, textStatus, errorThrown) {
+		   		console.log("Failed to make call to endpoint");
+		   		console.error("Status: " + textStatus);
+		   		console.error("Error: " + errorThrown);
+		   	}
+
+		}) 
+	} else {
+		console.log("schedule already loaded");
+	}
 }
